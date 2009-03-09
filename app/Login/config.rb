17be0e5f2ba12f513a21fe475a3f57ef6b28fb51
Vidal Graupera <vidal@vdggroup.com>
2009-03-09 15:58:47 -0700

    fix for autoregister

--- app/Login/config.rb
+++ app/Login/config.rb
@@ -1,4 +1,4 @@
 require 'rho'
 require File.join(__rhoGetCurrentDir(), 'apps','app','config/environment')
 
-Rho::RhoConfig::add_source("Login", {"url"=>"#{SYNC_SERVER}Login", "source_id"=>8})
+Rho::RhoConfig::add_source("Login", {"url"=>"#{SYNC_SERVER}rho_credential", "source_id"=>8})
